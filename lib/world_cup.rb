6d47ce0c970163d01e907e4c7285ed5bdd0c1357
Blake Enyart <blake.enyart@gmail.com>
2019-02-03 11:08:38 -0700

    Completion of iteration 4

--- lib/world_cup.rb
+++ lib/world_cup.rb
@@ -1,2 +1,28 @@
 class WorldCup
+
+  attr_reader :year,
+              :teams
+
+  def initialize(year, teams)
+    @year = year
+    @teams = teams
+  end
+
+  def active_players_by_position(position)
+    play_pos = []
+    @teams.map do |team|
+      if !team.eliminated?
+        play_pos << team.players_by_position(position)
+      end
+    end
+    play_pos.flatten!
+  end
+
+  def all_players_by_position
+    all_players = []
+    @teams.map { |team| all_players << team.players  }
+    all_players.flatten!
+    all_players = all_players.group_by { |player| player.position }
+    all_players.each_value { |player_list| player_list.sort_by!(&:name) }
+  end
 end
--- test/world_cup_test.rb
+++ test/world_cup_test.rb
@@ -11,12 +11,34 @@ class WorldCupTest < Minitest::Test
     @france = Team.new("France")
     @mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
     @pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
+    @france.add_player(@mbappe)
+    @france.add_player(@pogba)
 
-    @croatia = 
+    @croatia = Team.new("Croatia")
+    @modric = Player.new({name: "Luka Modric", position: "midfielder"})
+    @vida = Player.new({name: "Domagoj Vida", position: "defender"})
+    @croatia.add_player(@modric)
+    @croatia.add_player(@vida)
+
+    @world_cup = WorldCup.new(2018, [@france, @croatia])
   end
 
   def test_it_exist
 
     assert_instance_of WorldCup, @world_cup
+  end
+
+  def test_active_players_by_position
+
+    assert_equal [@pogba, @modric], @world_cup.active_players_by_position("midfielder")
+    @croatia.eliminate = true
+
+    assert_equal [@pogba], @world_cup.active_players_by_position("midfielder")
+  end
+
+  def test_all_players_by_position
+    expected = {"forward" => [@mbappe], 'midfielder' => [@modric, @pogba], 'defender' => [@vida] }
+
+    assert_equal expected,  @world_cup.all_players_by_position
   end
 end
